0ffe8f37d47f55d8447c936f4ef7e67213cac33d
nidhigothwal <165782089+nidhigothwal@users.noreply.github.com>
2024-10-07 20:24:12 +0530

    calculator updates

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-
-console.log('js is working')
-
+console.log('js is working');
 
 function add() {
-    console.log('Add function')
+    console.log('add function')
 
     let num1 = parseFloat(document.querySelector('.number1').value)
     let num2 = parseFloat(document.querySelector('.number2').value)
     let result = num1 + num2
-    document.querySelector('#result').innerText = result
+    document.querySelector('#result') .innerText = result
 }
 
 function sub() {
-    console.log('Subtract function')
+    console.log('sub function')
 
     let num1 = parseFloat(document.querySelector('.number1').value)
     let num2 = parseFloat(document.querySelector('.number2').value)
     let result = num1 - num2
-    document.querySelector('#result').innerText = result
+    document.querySelector('#result') .innerText = result
 }
-function multiply() {
-    console.log('Multiply function')
+function multi() {
+    console.log('multi function')
 
     let num1 = parseFloat(document.querySelector('.number1').value)
     let num2 = parseFloat(document.querySelector('.number2').value)
     let result = num1 * num2
-    document.querySelector('#result').innerText = result
+    document.querySelector('#result') .innerText = result
 }
 function divide() {
-    console.log('Divide function')
+    console.log('divide function')
 
     let num1 = parseFloat(document.querySelector('.number1').value)
     let num2 = parseFloat(document.querySelector('.number2').value)
     let result = num1 / num2
-    document.querySelector('#result').innerText = result
+    document.querySelector('#result') .innerText = result
 }
