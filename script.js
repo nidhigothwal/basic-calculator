773c95631fe0fc82661d307624d3dabe1043d46d
nidhigothwal <165782089+nidhigothwal@users.noreply.github.com>
2024-10-08 13:28:41 +0530

    look change  web page

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ console.log('js is working');
 function add() {
     console.log('add function')
 
-    let num1 = parseFloat(document.querySelector('.number1').value)
-    let num2 = parseFloat(document.querySelector('.number2').value)
-    let result = num1 + num2
+    let value1 = parseFloat(document.querySelector('.number1').value)
+    let value2 = parseFloat(document.querySelector('.number2').value)
+    let result = value1 + value2
     document.querySelector('#result') .innerText = result
 }
 
 function sub() {
     console.log('sub function')
 
-    let num1 = parseFloat(document.querySelector('.number1').value)
-    let num2 = parseFloat(document.querySelector('.number2').value)
-    let result = num1 - num2
+    let value1 = parseFloat(document.querySelector('.number1').value)
+    let value2 = parseFloat(document.querySelector('.number2').value)
+    let result = value1 - value2
     document.querySelector('#result') .innerText = result
 }
 function multi() {
     console.log('multi function')
 
-    let num1 = parseFloat(document.querySelector('.number1').value)
-    let num2 = parseFloat(document.querySelector('.number2').value)
-    let result = num1 * num2
+    let value1 = parseFloat(document.querySelector('.number1').value)
+    let value2 = parseFloat(document.querySelector('.number2').value)
+    let result = value1 * value2
     document.querySelector('#result') .innerText = result
 }
 function divide() {
     console.log('divide function')
 
-    let num1 = parseFloat(document.querySelector('.number1').value)
-    let num2 = parseFloat(document.querySelector('.number2').value)
-    let result = num1 / num2
+    let value1 = parseFloat(document.querySelector('.number1').value)
+    let value2 = parseFloat(document.querySelector('.number2').value)
+    let result = value1 / value2
     document.querySelector('#result') .innerText = result
 }
